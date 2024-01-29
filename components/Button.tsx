11d43f8e7db87f1d8d9a11133ba723275aee40fe
TheClashFruit <clash.fruit123@gmail.com>
2024-01-29 22:10:58 +0100

    feat: switch to ts

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,12 @@
 import styles from '@/styles/components/Button.module.scss';
 
+// @ts-ignore
 export default function Button({ icon: Icon, className, type, children, ...props }) {
   return (
     <>
       <button className={className ? `${styles.btn} ${className}` : styles.btn} type={type} {...props}>
         <span  className={styles.text}>{children}</span>
-        {Icon && <Icon className={styles.icon} height={18} width={18} />}
+        {Icon && <Icon className={styles.icon} height={20} width={20} />}
       </button>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -28,6 +28,8 @@ export default function Home() {
         Home
       </Button>
 
+
+
       <Dropdown items={[ { type: 'link', icon: Tv, href: '/', name: 'My Channel' }, { type: 'separator' }, { type: 'link', icon: Bell, href: '/', name: 'Notifications' }, { type: 'separator' }, { type: 'link', icon: Star, href: '/', name: 'Subscriptions' }, { type: 'link', icon: Wallet2, href: '/', name: 'Wallet' }, { type: 'separator' }, { type: 'link', icon: Settings, href: '/', name: 'Settings' }, { type: 'link', icon: Languages, href: '/', name: 'Language' }, { type: 'link', icon: SwatchBook, href: '/', name: 'Theme' }, { type: 'link', icon: Cookie, href: '/', name: 'Cookie Choices' }, { type: 'separator' }, { type: 'link', icon: LogOut, href: '/', name: 'Log Out' } ]} id="profileDropdown" picture={'https://cdn.zleed.tv/brand/logo_dark.svg'}>TheClashFruit</Dropdown>
       <Dropdown items={[ { type: 'link', icon: HomeIcon, href: '/', name: 'Home' } ]} id="generic">generic dropdown</Dropdown>
     </>
